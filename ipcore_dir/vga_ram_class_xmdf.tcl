b6c8cf3692fa34eeecea2de3fb8ac5975bbf4ce9
David Protzman <dmp250net@gmail.com>
2015-02-07 18:39:23 -0500

    This code has an error that is causing it not to display anything on the VGA monitor

---
+++ ipcore_dir/vga_ram_class_xmdf.tcl
@@ -0,0 +1,251 @@
+# The package naming convention is <core_name>_xmdf
+package provide vga_ram_class_xmdf 1.0
+
+# This includes some utilities that support common XMDF operations
+package require utilities_xmdf
+
+# Define a namespace for this package. The name of the name space
+# is <core_name>_xmdf
+namespace eval ::vga_ram_class_xmdf {
+# Use this to define any statics
+}
+
+# Function called by client to rebuild the params and port arrays
+# Optional when the use context does not require the param or ports
+# arrays to be available.
+proc ::vga_ram_class_xmdf::xmdfInit { instance } {
+# Variable containing name of library into which module is compiled
+# Recommendation: <module_name>
+# Required
+utilities_xmdf::xmdfSetData $instance Module Attributes Name vga_ram_class
+}
+# ::vga_ram_class_xmdf::xmdfInit
+
+# Function called by client to fill in all the xmdf* data variables
+# based on the current settings of the parameters
+proc ::vga_ram_class_xmdf::xmdfApplyParams { instance } {
+
+set fcount 0
+# Array containing libraries that are assumed to exist
+# Examples include unisim and xilinxcorelib
+# Optional
+# In this example, we assume that the unisim library will
+# be available to the simulation and synthesis tool
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type logical_library
+utilities_xmdf::xmdfSetData $instance FileSet $fcount logical_library unisim
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path summary.log
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type AnyView
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path vga_ram_class/blk_mem_gen_v7_3_readme.txt
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type Ignore
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path vga_ram_class/doc/blk_mem_gen_v7_3_vinfo.html
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type Ignore
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path vga_ram_class/doc/pg058-blk-mem-gen.pdf
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type Ignore
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path vga_ram_class/example_design/vga_ram_class_exdes.ucf
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type Ignore
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path vga_ram_class/example_design/vga_ram_class_exdes.vhd
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type Ignore
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path vga_ram_class/example_design/vga_ram_class_exdes.xdc
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type Ignore
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path vga_ram_class/example_design/vga_ram_class_prod.vhd
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type Ignore
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path vga_ram_class/implement/implement.bat
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type Ignore
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path vga_ram_class/implement/implement.sh
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type Ignore
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path vga_ram_class/implement/planAhead_ise.bat
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type Ignore
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path vga_ram_class/implement/planAhead_ise.sh
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type Ignore
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path vga_ram_class/implement/planAhead_ise.tcl
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type Ignore
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path vga_ram_class/implement/xst.prj
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type Ignore
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path vga_ram_class/implement/xst.scr
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type Ignore
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path vga_ram_class/simulation/addr_gen.vhd
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type Ignore
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path vga_ram_class/simulation/bmg_stim_gen.vhd
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type Ignore
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path vga_ram_class/simulation/bmg_tb_pkg.vhd
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type Ignore
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path vga_ram_class/simulation/checker.vhd
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type Ignore
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path vga_ram_class/simulation/data_gen.vhd
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type Ignore
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path vga_ram_class/simulation/functional/simcmds.tcl
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type Ignore
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path vga_ram_class/simulation/functional/simulate_isim.sh
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type Ignore
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path vga_ram_class/simulation/functional/simulate_mti.bat
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type Ignore
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path vga_ram_class/simulation/functional/simulate_mti.do
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type Ignore
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path vga_ram_class/simulation/functional/simulate_mti.sh
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type Ignore
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path vga_ram_class/simulation/functional/simulate_ncsim.sh
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type Ignore
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path vga_ram_class/simulation/functional/simulate_vcs.sh
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type Ignore
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path vga_ram_class/simulation/functional/ucli_commands.key
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type Ignore
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path vga_ram_class/simulation/functional/vcs_session.tcl
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type Ignore
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path vga_ram_class/simulation/functional/wave_mti.do
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type Ignore
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path vga_ram_class/simulation/functional/wave_ncsim.sv
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type Ignore
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path vga_ram_class/simulation/random.vhd
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type Ignore
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path vga_ram_class/simulation/timing/simcmds.tcl
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type Ignore
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path vga_ram_class/simulation/timing/simulate_isim.sh
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type Ignore
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path vga_ram_class/simulation/timing/simulate_mti.bat
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type Ignore
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path vga_ram_class/simulation/timing/simulate_mti.do
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type Ignore
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path vga_ram_class/simulation/timing/simulate_mti.sh
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type Ignore
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path vga_ram_class/simulation/timing/simulate_ncsim.sh
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type Ignore
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path vga_ram_class/simulation/timing/simulate_vcs.sh
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type Ignore
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path vga_ram_class/simulation/timing/ucli_commands.key
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type Ignore
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path vga_ram_class/simulation/timing/vcs_session.tcl
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type Ignore
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path vga_ram_class/simulation/timing/wave_mti.do
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type Ignore
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path vga_ram_class/simulation/timing/wave_ncsim.sv
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type Ignore
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path vga_ram_class/simulation/vga_ram_class_synth.vhd
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type Ignore
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path vga_ram_class/simulation/vga_ram_class_tb.vhd
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type Ignore
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path vga_ram_class.asy
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type asy
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path vga_ram_class.ngc
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type ngc
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path vga_ram_class.sym
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type symbol
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path vga_ram_class.vhd
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type vhdl
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path vga_ram_class.vho
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type vhdl_template
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path vga_ram_class.xco
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type coregen_ip
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path vga_ram_class_xmdf.tcl
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type AnyView
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount associated_module vga_ram_class
+incr fcount
+
+}
+
+# ::gen_comp_name_xmdf::xmdfApplyParams
--- master.vhd
+++ master.vhd
@@ -7,14 +7,125 @@ entity master is
     Port ( clk : in  STD_LOGIC;
            rst : in  STD_LOGIC;
            rx : in  STD_LOGIC;
-           sine_out : out  STD_LOGIC_VECTOR (7 downto 0));
+           sine_out : out  STD_LOGIC_VECTOR (7 downto 0);
+			  adc_in : in STD_LOGIC_VECTOR(7 downto 0);
+			  adc_clk : out STD_LOGIC;
+			  vga_hs : out STD_LOGIC;
+			  vga_vs : out STD_LOGIC;
+			  vga_red : out STD_LOGIC_VECTOR(2 downto 0);
+			  vga_green : out STD_LOGIC_VECTOR(2 downto 0);
+			  vga_blue : out STD_LOGIC_VECTOR(1 downto 0));
 end master;
 
 architecture Behavioral of master is
 
+	signal last_vga_v_blanking, vga_h_blanking, vga_v_blanking : std_logic;
+	signal vga_red_in, vga_green_in : std_logic_vector(2 downto 0);
+	signal vga_blue_in : std_logic_vector(1 downto 0);
+	signal vga_x_pos, vga_y_pos : integer;
+	
+	COMPONENT vga_configurable
+	PORT(
+		clk : IN std_logic;
+		rst : IN std_logic;
+		blue_in : IN std_logic_vector(1 downto 0);
+		red_in : IN std_logic_vector(2 downto 0);
+		green_in : IN std_logic_vector(2 downto 0);          
+		hs : OUT std_logic;
+		vs : OUT std_logic;
+		green : OUT std_logic_vector(2 downto 0);
+		red : OUT std_logic_vector(2 downto 0);
+		blue : OUT std_logic_vector(1 downto 0);
+		v_blanking : OUT std_logic;
+		h_blanking : OUT std_logic;
+		x_pos : OUT integer;
+		y_pos : OUT integer
+		);
+	END COMPONENT;
+
+	signal vga_ram_class_enb : std_logic;
+	signal vga_ram_class_addrb : std_logic_vector(8 downto 0);
+	signal vga_ram_class_doutb : std_logic_vector(7 downto 0);
+	COMPONENT vga_ram_class
+	  PORT (
+		 clka : IN STD_LOGIC;
+		 ena : IN STD_LOGIC;
+		 wea : IN STD_LOGIC_VECTOR(0 DOWNTO 0);
+		 addra : IN STD_LOGIC_VECTOR(8 DOWNTO 0);
+		 dina : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+		 clkb : IN STD_LOGIC;
+		 enb : IN STD_LOGIC;
+		 addrb : IN STD_LOGIC_VECTOR(8 DOWNTO 0);
+		 doutb : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)
+	  );
+	END COMPONENT;
+
+	signal mag_output_valid : std_logic;
+	signal mag_mag : std_logic_vector(7 downto 0);
+	signal mag_idx_out : std_logic_vector(8 downto 0);
+	COMPONENT mag
+	PORT(
+		clk : IN std_logic;
+		rst : IN std_logic;
+		input_valid : IN std_logic;
+		re : IN std_logic_vector(7 downto 0);
+		im : IN std_logic_vector(7 downto 0);
+		idx_in : IN std_logic_vector(8 downto 0);          
+		idx_out : OUT std_logic_vector(8 downto 0);
+		mag : OUT std_logic_vector(7 downto 0);
+		output_valid : OUT std_logic
+		);
+	END COMPONENT;
+
+	signal fft_clk, original_clk, vga_clk : std_logic;
+	component dcm_class
+		port
+		 (
+		  clk : in std_logic;
+		  fft_clk : out std_logic;
+		  vga_clk : out std_logic;
+		  original_clk : out std_logic
+		 );
+		end component;
+
+	signal fft_xk_re, fft_xk_im : std_logic_vector(7 downto 0);
+	signal fft_xk_index, fft_xn_index : std_logic_vector(8 downto 0);
+	signal fft_dv, fft_ce, fft_start, fft_done, fft_unload, fft_rfd, fft_busy, fft_ovflo : std_logic;
+	signal fft_scale_sch : std_logic_vector(17 downto 0);
+	signal fft_waiting_to_write : std_logic;
+	COMPONENT fft_class
+	  PORT (
+		 clk : IN STD_LOGIC;
+		 ce : IN STD_LOGIC;
+		 sclr : IN STD_LOGIC;
+		 start : IN STD_LOGIC;
+		 unload : IN STD_LOGIC;
+		 xn_re : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+		 xn_im : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+		 fwd_inv : IN STD_LOGIC;
+		 fwd_inv_we : IN STD_LOGIC;
+		 scale_sch : IN STD_LOGIC_VECTOR(17 DOWNTO 0);
+		 scale_sch_we : IN STD_LOGIC;
+		 rfd : OUT STD_LOGIC;
+		 xn_index : OUT STD_LOGIC_VECTOR(8 DOWNTO 0);
+		 busy : OUT STD_LOGIC;
+		 edone : OUT STD_LOGIC;
+		 done : OUT STD_LOGIC;
+		 dv : OUT STD_LOGIC;
+		 xk_index : OUT STD_LOGIC_VECTOR(8 DOWNTO 0);
+		 xk_re : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
+		 xk_im : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
+		 ovflo : OUT STD_LOGIC
+	  );
+	END COMPONENT;
+
 	signal frame : frame_t := (others => (others => '0'));
 	signal frame_valid : std_logic := '0';
 	COMPONENT uart_comms
+	GENERIC(
+		clk_rate : natural := 100_000_000;
+		baud_rate : natural := 500_000
+	);
 	PORT(
 		clk : IN std_logic;
 		rst : IN std_logic;
@@ -36,21 +147,75 @@ architecture Behavioral of master is
 	signal sine_buffer : std_logic_vector(sine_out'range) := (others => '0');
 
 begin
+
+	adc_clk <= fft_clk;
 	
-	process(clk, rst)
+	process(vga_clk, rst)
+	begin
+		if(rst = '1') then
+			vga_blue_in <= (others => '0');
+		elsif(rising_edge(vga_clk)) then
+			vga_ram_class_enb <= '0';
+		
+			if(vga_x_pos >= 0 and vga_x_pos < 512 
+					and vga_y_pos >= 0 and vga_y_pos < 256) then
+				vga_ram_class_enb <= '1';
+				vga_ram_class_addrb <= 
+					std_logic_vector(to_unsigned(vga_x_pos, vga_ram_class_addrb'length));
+				
+				if(unsigned(vga_ram_class_doutb) = vga_y_pos) then
+					vga_blue_in <= (others => '1');
+				else
+					vga_blue_in <= (others => '0');
+				end if;
+			else
+				vga_blue_in <= (others => '0');
+			end if;
+		end if;
+	end process;
+	
+	process(fft_clk, rst)
+	begin
+		if(rst = '1') then
+			fft_start <= '0';
+		elsif(rising_edge(fft_clk)) then
+			fft_start <= '0';
+			fft_unload <= '0';
+			last_vga_v_blanking <= vga_v_blanking;
+			
+			if(fft_done = '1') then
+				fft_waiting_to_write <= '1';
+			end if;
+			
+			if(last_vga_v_blanking = '0' and vga_v_blanking = '1' 
+					and fft_waiting_to_write = '1') then
+				fft_unload <= '1';
+				fft_waiting_to_write <= '0';
+			end if;
+			
+			if(fft_rfd = '0' and fft_start = '0' and fft_busy = '0' 
+				and fft_done = '0' and fft_dv = '0' and fft_unload = '0'
+				and vga_v_blanking = '0') then
+					
+					fft_start <= '1';
+			end if;
+		end if;
+	end process;
+	
+	process(fft_clk, rst)
 	begin
 		if(rst = '1') then
 			sine_out <= x"80";
-		elsif(rising_edge(clk)) then
+		elsif(rising_edge(fft_clk)) then
 			sine_out <= std_logic_vector(unsigned(sine_buffer) + 128);
 		end if;
 	end process;
 	
-	process(clk, rst)
+	process(original_clk, rst)
 	begin
 		if(rst = '1') then
 			pinc_in <= x"00FF";
-		elsif(rising_edge(clk)) then
+		elsif(rising_edge(original_clk)) then
 			if(frame_valid = '1') then
 				case frame(3) is
 					when x"00" =>
@@ -64,17 +229,91 @@ begin
 
 	sine_gen_inst : sine_gen
 	  PORT MAP (
-		 clk => clk,
+		 clk => fft_clk,
 		 pinc_in => pinc_in,
 		 sine => sine_buffer
 	  );
 	  
 	Inst_uart_comms: uart_comms PORT MAP(
-		clk => clk,
+		clk => original_clk,
 		rst => rst,
 		rx => rx,
 		frame => frame,
 		frame_valid => frame_valid
+	);
+	
+	fft_class_inst : fft_class
+	  PORT MAP (
+		 clk => fft_clk,
+		 ce => fft_ce,
+		 sclr => rst,
+		 start => fft_start,
+		 unload => fft_unload,
+		 xn_re => adc_in,
+		 xn_im => (others => '0'),
+		 fwd_inv => '1',
+		 fwd_inv_we => '1',
+		 scale_sch => fft_scale_sch,
+		 scale_sch_we => '1',
+		 rfd => fft_rfd,
+		 xn_index => open,
+		 busy => fft_busy,
+		 edone => open,
+		 done => fft_done,
+		 dv => fft_dv,
+		 xk_index => fft_xk_index,
+		 xk_re => fft_xk_re,
+		 xk_im => fft_xk_im,
+		 ovflo => fft_ovflo
+	  );
+	  
+	  dcm_class_inst : dcm_class
+		  port map
+			( clk => clk,
+			  fft_clk => fft_clk,
+			  vga_clk => vga_clk,
+			  original_clk => original_clk);
+	 
+	 Inst_mag: mag PORT MAP(
+		clk => fft_clk,
+		rst => rst,
+		input_valid => fft_dv,
+		re => fft_xk_re,
+		im => fft_xk_im,
+		idx_in => fft_xk_index,
+		idx_out => mag_idx_out,
+		mag => mag_mag,
+		output_valid => mag_output_valid
+	);
+	
+	vga_ram_inst : vga_ram_class
+	  PORT MAP (
+		 clka => fft_clk,
+		 ena => mag_output_valid,
+		 wea => (others => mag_output_valid),
+		 addra => mag_idx_out,
+		 dina => mag_mag,
+		 clkb => vga_clk,
+		 enb => vga_ram_class_enb,
+		 addrb => vga_ram_class_addrb,
+		 doutb => vga_ram_class_doutb
+	  );
+	  
+	Inst_vga_configurable: vga_configurable PORT MAP(
+		clk => vga_clk,
+		rst => rst,
+		hs => vga_hs,
+		vs => vga_vs,
+		green => vga_green,
+		red => vga_red,
+		blue => vga_blue,
+		v_blanking => vga_v_blanking,
+		h_blanking => vga_h_blanking,
+		x_pos => vga_x_pos,
+		y_pos => vga_y_pos,
+		blue_in => vga_blue_in,
+		red_in => vga_red_in,
+		green_in => vga_green_in
 	);
 
 end Behavioral;
